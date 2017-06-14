143eadaabd1e9490b4cd9e72a039ea0a662058e6
Uilian Ries <uilianries@gmail.com>
2017-06-14 12:17:57 -0300

    Add MinGW support
    - Test Catch in MinGW
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- build.py
+++ build.py
@@ -4,7 +4,11 @@ from conan.packager import ConanMultiPackager
 
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager()
+    mingw_configurations = [("4.9", "x86_64", "seh", "posix"),
+                            ("4.9", "x86_64", "sjlj", "posix"),
+                            ("4.9", "x86", "sjlj", "posix"),
+                            ("4.9", "x86", "dwarf2", "posix")]
+    builder = ConanMultiPackager(mingw_configurations=mingw_configurations)
     builder.password = getenv("CONAN_PASSWORD")
     builder.add_common_builds(pure_c=False)
     builder.run()
