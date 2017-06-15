52eece1b4718592162e8e5ffeb58d4e1d531dbf1
Uilian Ries <uilianries@gmail.com>
2017-06-15 10:16:07 -0300

    Update job matrix for CI
    - Apply job template for Travis CI
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- build.py
+++ build.py
@@ -4,11 +4,7 @@ from conan.packager import ConanMultiPackager
 
 
 if __name__ == "__main__":
-    mingw_configurations = [("6.3", "x86_64", "seh", "posix"),
-                            ("6.3", "x86_64", "sjlj", "posix"),
-                            ("6.3", "x86", "sjlj", "posix"),
-                            ("6.3", "x86", "dwarf2", "posix")]
-    builder = ConanMultiPackager(mingw_configurations=mingw_configurations)
+    builder = ConanMultiPackager()
     builder.password = getenv("CONAN_PASSWORD")
     builder.add_common_builds(pure_c=False)
     builder.run()
